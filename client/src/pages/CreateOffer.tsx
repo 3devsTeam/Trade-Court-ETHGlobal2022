aa73ae512295ad2c275c58e8a417d256638035f8
Vasily <v5mirnov03@yandex.ru>
2022-09-13 19:39:04 +0300

    add progressbar

--- client/src/pages/CreateOffer.tsx
+++ client/src/pages/CreateOffer.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Form } from "../components/create-offer/Form";
 import { FormNav } from "../components/create-offer/FormNav";
 import { Preview } from "../components/create-offer/Preview";
+import { Progressbar } from "../components/create-offer/Progressbar";
 
 export const CreateOffer = () => {
   return (
     <>
+      <Progressbar />
       <div className={"grid grid-cols-2 gap-5"}>
         <Form />
         <Preview />
