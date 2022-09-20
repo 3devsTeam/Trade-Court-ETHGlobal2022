c32a1ad8273e5655a4af38fe62a83aa8ffca607a
Vasily <v5mirnov03@yandex.ru>
2022-09-20 17:26:36 +0300

    before pull

--- client/src/pages/Transaction.tsx
+++ client/src/pages/Transaction.tsx
@@ -1,8 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { useParams } from "react-router-dom";
 import { OfferService } from "../services/offer.services";
 
 export const Transaction = () => {
+  let { transaction } = useParams();
+  console.log(transaction);
+
   const {} = useQuery(["get offer by id", () => OfferService.getByID("")]);
 
   return <div>Transaction</div>;
