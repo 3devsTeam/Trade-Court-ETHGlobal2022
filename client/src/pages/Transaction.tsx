fcd55410af5f31abb11f3b61dd2312fa8659bc4b
Vasily <v5mirnov03@yandex.ru>
2022-09-20 16:45:28 +0300

    fix npm modules conflict => add reactquery devtools

--- client/src/pages/Transaction.tsx
+++ client/src/pages/Transaction.tsx
@@ -1,5 +1,9 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { OfferService } from "../services/offer.services";
 
 export const Transaction = () => {
+  const {} = useQuery(["get offer by id", () => OfferService.getByID("")]);
+
   return <div>Transaction</div>;
 };
