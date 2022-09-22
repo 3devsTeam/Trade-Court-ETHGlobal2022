357abfcadeb436f0d83386da85477254840bbd31
Leonid Zotov <zotovleonid@icloud.com>
2022-09-22 14:05:51 +0300

    add socket base setup

--- server/socket.js
+++ server/socket.js
@@ -1,4 +1,18 @@
 module.exports = (io, socket) => {
-  console.log("socket connected");
-  socket.emit("msg", "piski");
+  socket.on('joinOffer', (data) => {
+    socket.join(data.offerId);
+  });
+  socket.on('changeStage', (offerId) => {
+    io.to(offerId).emit('newStage');
+  });
 };
+
+// const socket = io('http://127.0.0.1:3030');
+
+// socket.on('msg', (data) => console.log(data));
+// api/joinRoom получешь саксес
+//заходит socket.emit('joinOffer', {userId, offerId})
+
+//socket.emit('changeStage', offerId)
+
+//socket.on('newStage', ()=>{обонвляешь компонент})
