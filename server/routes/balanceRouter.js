a959e963a29a7e0174b6e51c9e90ee19b67e1fa2
Leonid Zotov <zotovleonid@icloud.com>
2022-09-23 19:51:44 +0300

    add balance query for optimism polygon

--- server/routes/balanceRouter.js
+++ server/routes/balanceRouter.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const balanceController = require('../controllers/balanceController');
+const balanceControllerEthereum = require('../controllers/balanceController/ethereum/balanceController');
+const balanceControllerPolygon = require('../controllers/balanceController/polygon/balanceController');
+const balanceControllerOptimism = require('../controllers/balanceController/optimism/balanceController');
 
 const router = express.Router();
 
-router.route('/erc20/:address').get(balanceController.getERC20Balances);
-router.route('/rate/erc20').get(balanceController.getERC20Rate);
-router.route('/tokens/:url').get(balanceController.getTokenImg);
+router.route('/ethereum/:address').get(balanceControllerEthereum.getBalance);
+router.route('/rate/ethereum').get(balanceControllerEthereum.getRate);
 
+router.route('/polygon/:address').get(balanceControllerPolygon.getBalance);
+router.route('/rate/polygon').get(balanceControllerPolygon.getRate);
+
+router.route('/optimism/:address').get(balanceControllerOptimism.getBalance);
+router.route('/rate/optimism').get(balanceControllerOptimism.getRate);
+// router.route('/tokens/:url').get(balanceController.getTokenImg);
 module.exports = router;
