b4539cc2e27f18feaf1ba805be6c3e79f80ff489
Leonid Zotov <zotovleonid@icloud.com>
2022-10-05 19:05:23 +0300

    add initial contract setup

--- client/src/components/modal/OfferModal.tsx
+++ client/src/components/modal/OfferModal.tsx
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from "react";
-import { truncateAddress } from "../../utils/truncateAddress";
-import { Input } from "../create-offer/Input";
-import { useNavigate } from "react-router";
-import { IOffer } from "../../models/models";
-import { OfferInput } from "../home/OfferInput";
-import { OfferService } from "../../services/offer.services";
-import { toast } from "react-toastify";
-import { useEthContract } from "../../hooks/useEthContract";
-import { parseEther } from "../../utils/parseEther";
-import { round } from "../../utils/round";
+import React, { useState, useEffect } from 'react';
+import { truncateAddress } from '../../utils/truncateAddress';
+import { Input } from '../create-offer/Input';
+import { useNavigate } from 'react-router';
+import { IOffer } from '../../models/models';
+import { OfferInput } from '../home/OfferInput';
+import { OfferService } from '../../services/offer.services';
+import { toast } from 'react-toastify';
+import { useEthContract } from '../../hooks/useEthContract';
+import { parseEther } from '../../utils/parseEther';
+import { round } from '../../utils/round';
 
 interface IOfferModalProps {
   close: any;
@@ -25,7 +25,6 @@ export const OfferModal = ({ close, offer }: IOfferModalProps) => {
     quantity,
     offerComment,
     orderLimit,
-    room,
   } = offer;
 
   const navigate = useNavigate();
@@ -43,7 +42,7 @@ export const OfferModal = ({ close, offer }: IOfferModalProps) => {
 
   const args = [+roomId, parseEther(recieve.toString())];
   const value = 0;
-  const functionName = "completeDeal";
+  const functionName = 'completeDeal';
 
   const { data, prepareError, isError, isLoading, isSuccess, writeAsync } =
     useEthContract(args, functionName);
@@ -64,7 +63,7 @@ export const OfferModal = ({ close, offer }: IOfferModalProps) => {
         amount: pay,
       })
         .then((data) => {
-          if (data.data.message === "success") {
+          if (data.data.message === 'success') {
             close();
             navigate(`/transaction/${_id}`);
           }
@@ -77,60 +76,60 @@ export const OfferModal = ({ close, offer }: IOfferModalProps) => {
 
   const info = [
     {
-      name: "Maker:",
+      name: 'Maker:',
       value: truncateAddress(address),
     },
     {
-      name: "Unit Price:",
+      name: 'Unit Price:',
       value: `${unitPrice} ${ticker}`,
     },
     {
-      name: "Available:",
+      name: 'Available:',
       value: `${round(quantity, 4)} ${symbol}`,
     },
     {
-      name: "Limit:",
+      name: 'Limit:',
       value: `${orderLimit[0]}-${orderLimit[1]} ${ticker}`,
     },
   ];
 
   return (
-    <div className="rounded-[15px] grid grid-cols-2">
-      <div className="flex flex-col gap-3 p-3 cursor-default border-2 border-purple rounded-[20px]">
+    <div className='rounded-[15px] grid grid-cols-2'>
+      <div className='flex flex-col gap-3 p-3 cursor-default border-2 border-purple rounded-[20px]'>
         {info.map((i) => (
-          <div className={"flex justify-between"}>
+          <div className={'flex justify-between'}>
             <p>{i.name}</p>
-            <p className={"font-bold"}>{i.value}</p>
+            <p className={'font-bold'}>{i.value}</p>
           </div>
         ))}
-        <div className="break-words bg-purple bg-opacity-20 p-2 rounded-[10px]">
-          <p className={"text-sm"}>{offerComment}</p>
+        <div className='break-words bg-purple bg-opacity-20 p-2 rounded-[10px]'>
+          <p className={'text-sm'}>{offerComment}</p>
         </div>
       </div>
 
-      <div className="flex flex-col gap-[10px] px-3">
+      <div className='flex flex-col gap-[10px] px-3'>
         <OfferInput
-          label={"You pay"}
+          label={'You pay'}
           maxValue={orderLimit[1]}
           setValue={setPay}
-          placeholder={"You pay"}
+          placeholder={'You pay'}
           value={pay}
           inputContent={ticker}
         />
         <OfferInput
           readOnly={false}
-          label={"You recieve"}
+          label={'You recieve'}
           setValue={setRecieve}
-          placeholder={"You recieve"}
+          placeholder={'You recieve'}
           value={recieve}
           inputContent={symbol}
         />
         <div>
           <button
             onClick={() => transactionHandler()}
-            className="bg-purple font-bold p-2 rounded-[20px] w-full h-[55px]"
+            className='bg-purple font-bold p-2 rounded-[20px] w-full h-[55px]'
           >
-            <span className="text-white">Buy {symbol}</span>
+            <span className='text-white'>Buy {symbol}</span>
           </button>
         </div>
       </div>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -62,7 +62,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <QueryClientProvider client={queryClient}>
           <App />
 
-          {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+          <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
       </Provider>
     </RainbowKitProvider>
