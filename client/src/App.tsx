ee7badfbefced248b5e2c2654fe9d1d66864609c
Vasily <v5mirnov03@yandex.ru>
2022-10-26 13:40:55 +0300

    last changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
-import { useState } from "react";
+import { Suspense, useState, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/navbar/Navbar";
-import { CreateOffer } from "./pages/CreateOffer";
 import { Home } from "./pages/Home";
 import { PrivateRoutes } from "./routes/PrivateRoutes";
 import { Settings } from "./pages/Settings";
@@ -11,22 +10,26 @@ import { Profile } from "./pages/Profile";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+const CreateOffer = lazy(() => import("./pages/CreateOffer"));
+
 const App = () => {
   return (
     <BrowserRouter>
       <Navbar />
       <main className='max-w-5xl mx-auto'>
-        <Routes>
-          {/* <Route element={<PrivateRoutes />}> */}
-          <Route path={"/create-offer"} element={<CreateOffer />} />
-          <Route path={"/settings"} element={<Settings />} />
-          <Route path={"/transaction/:id"} element={<Transaction />} />
-          <Route path={"/profile"} element={<Profile />} />
-          {/* </Route> */}
+        <Suspense fallback={<h1>loading...</h1>}>
+          <Routes>
+            {/* <Route element={<PrivateRoutes />}> */}
+            <Route path={"/create-offer"} element={CreateOffer} />
+            <Route path={"/settings"} element={<Settings />} />
+            <Route path={"/transaction/:id"} element={<Transaction />} />
+            <Route path={"/profile"} element={<Profile />} />
+            {/* </Route> */}
 
-          <Route path={"/"} element={<Home />} />
-          {/* <Route path={"*"} element={<Navigate to={"/"} />} /> */}
-        </Routes>
+            <Route path={"/"} element={<Home />} />
+            {/* <Route path={"*"} element={<Navigate to={"/"} />} /> */}
+          </Routes>
+        </Suspense>
       </main>
     </BrowserRouter>
   );
