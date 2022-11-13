5e9829f3c60c1d2b42d0c643ff91ddf06f9854bb
Vasily <v5mirnov03@yandex.ru>
2022-11-14 00:40:43 +0300

    remove: cors error

--- server/app.js
+++ server/app.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const indexRouter = require("./routes/index");
-const cookieParser = require("cookie-parser");
+const express = require("express")
+const morgan = require("morgan")
+const cors = require("cors")
+const indexRouter = require("./routes/index")
+const cookieParser = require("cookie-parser")
 
-const globalErrorHandler = require("./utils/errorController");
+const globalErrorHandler = require("./utils/errorController")
 
-const app = express();
+const app = express()
 app.use(
   cors({
     credentials: true,
-    origin: "http://127.0.0.1:5173",
+    origin: "http://localhost:5173",
   })
-);
-app.options("*", cors());
+)
+app.options("*", cors())
 
 if (process.env.NODE_ENV === "dev") {
-  app.use(morgan("dev"));
+  app.use(morgan("dev"))
 }
-app.use(cookieParser());
-app.use(express.json({ limit: "10kb" }));
-app.use("/api", indexRouter);
-app.use(globalErrorHandler);
+app.use(cookieParser())
+app.use(express.json({ limit: "10kb" }))
+app.use("/api", indexRouter)
+app.use(globalErrorHandler)
 
-module.exports = app;
+module.exports = app
