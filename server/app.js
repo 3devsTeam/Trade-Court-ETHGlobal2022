ccc1d205d1a51ce8f364a44e036d13dcdff4c4f9
Leonid Zotov <zotovleonid@icloud.com>
2022-11-17 04:14:39 +0300

    cookie bugfix

--- server/app.js
+++ server/app.js
@@ -8,8 +8,10 @@ const cookieParser = require('cookie-parser');
 const globalErrorHandler = require('./utils/errorController');
 
 const app = express();
-app.use('/api', indexRouter);
-app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.use(cookieParser());
+app.use(express.json({ limit: '10kb' }));
+app.use(globalErrorHandler);
 app.use(
   cors({
     origin: 'http://161.35.83.140:3030',
@@ -22,8 +24,18 @@ app.options('*', cors());
 if (process.env.NODE_ENV === 'dev') {
   app.use(morgan('dev'));
 }
-app.use(cookieParser());
-app.use(express.json({ limit: '10kb' }));
-app.use(globalErrorHandler);
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use('/api', indexRouter);
+app.get('/*', function (req, res) {
+  res.sendFile(
+    path.join(__dirname, '../client/dist/index.html'),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
 
 module.exports = app;
