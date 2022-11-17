12b4ae491e532c87d62891cfec54a28afc0b83f4
Vasily <v5mirnov03@yandex.ru>
2022-11-17 15:07:50 +0300

    fix: conflict

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,26 +3,6 @@ import react from '@vitejs/plugin-react'
 import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
-<<<<<<< HEAD
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, 'env')
-
-  return {
-    define: {
-      global: {},
-      'process.env': {}
-    },
-    plugins: [
-      react(),
-      legacy({
-        targets: ['defaults', 'not IE 11']
-      })
-    ],
-    build: {
-      outDir: 'build'
-    }
-  }
-=======
 export default defineConfig({
   define: {
     global: {},
@@ -38,5 +18,4 @@ export default defineConfig({
     })
   ],
   envDir: '../../../../conffiles/'
->>>>>>> ccc1d205d1a51ce8f364a44e036d13dcdff4c4f9
 })
